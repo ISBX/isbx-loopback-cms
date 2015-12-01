335df0d604637dbc9f9d5382ac427c3536e8961c
Kelly Chu <kchu@isbx.com>
2015-12-01 13:47:01 -0800

    remove the UTC dependency on the Date Time Picker

--- client/common/directives/DateTimePicker/DateTimePicker.js
+++ client/common/directives/DateTimePicker/DateTimePicker.js
@@ -32,7 +32,7 @@ angular.module('dashboard.directive.DateTimePicker', [
         elem.datetimepicker({
           format: scope.format,
           useCurrent: false,
-          defaultDate: scope.defaultDate ? moment.utc(scope.defaultDate, scope.format) : undefined,
+          defaultDate: scope.defaultDate ? moment(scope.defaultDate, scope.format) : undefined,
           viewMode: scope.viewMode,
           widgetPositioning: { horizontal: scope.horizontal ? scope.horizontal : 'auto' }
         });
@@ -46,7 +46,7 @@ angular.module('dashboard.directive.DateTimePicker', [
         //On Blur update the ng-model
         elem.on('blur', function () {
           if (!scope.format) scope.format = scope.ngFormat;
-          var dateValue = moment.utc(elem.val(), scope.format);
+          var dateValue = moment(elem.val(), scope.format);
           if (dateValue.isValid()) {
             ngModel.$setViewValue(dateValue);
           } else {
