b6cd66d8e837095f6d162dbdaa56441d65b94bc2
Jacky Lao <jacquelineraelao@yahoo.com>
2017-01-13 17:56:40 +0800

    Remove unnecessary quotes and fix date formatting

--- client/common/directives/DateTimePicker/DateTimePicker.js
+++ client/common/directives/DateTimePicker/DateTimePicker.js
@@ -27,12 +27,14 @@ angular.module('dashboard.directive.DateTimePicker', [
           if (!value) return;
           return moment(value).format(scope.format);
         });
-          
+        
+        scope.defaultDate = scope.defaultDate ? scope.defaultDate.replace(/"/g, '') : scope.defaultDate; //remove quotes  
+        
         //Bind the Element
         elem.datetimepicker({
           format: scope.format,
           useCurrent: false,
-          defaultDate: scope.defaultDate ? moment(scope.defaultDate, scope.format) : undefined,
+          defaultDate: scope.defaultDate ? moment(scope.defaultDate).format(scope.format) : undefined,
           viewMode: scope.viewMode,
           widgetPositioning: { horizontal: scope.horizontal ? scope.horizontal : 'auto' }
         });
