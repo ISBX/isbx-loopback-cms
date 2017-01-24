f0277a55226ddf2a79f324dfadcedb93fd6ee53d
Mike <mgregorio@isbx.com>
2017-01-24 17:46:30 +0800

    restore not needed changes

--- index.js
+++ index.js
@@ -318,12 +318,6 @@ function cms(loopbackApplication, options) {
     });
   });
 
-  if (config.allowUnsafeUpsert) {
-    console.warn('Warning (isbx-loopback-cms): /model/save end point is running in compatibility mode, please update your ACL rules asap then remove config.allowUnsafeUpsert or set it to false');
-  } else {
-    console.warn('Warning (isbx-loopback-cms): /model/save end point is running in secure mode. This can potentially break your application if have not updated your ACL rules. For backwards compatibility, you may set config.allowUnsafeUpsert to true in your CMS Config if you are not ready to update your ACL rules yet.');
-  }
-
   function validateToken(request, callback) {
     var AccessToken = loopbackApplication.models.AccessToken;
     var tokenString = request.body.__accessToken || request.query.access_token;
@@ -344,34 +338,13 @@ function cms(loopbackApplication, options) {
     validateToken(req, function(err, isValid, token) {
       if (err) { return res.status(500).send(err); }
       if (!isValid) { return res.status(403).send('Forbidden'); }
-
-      var data = req.body;
-      var context = {
-        accessToken: token,
-        model: data.__model,
-        property: data.__id ? 'updateAttributes' : 'create',
-        modelId: data.__id || null
-      };
-
-      function upsertData() {
-        relationalUpsert.upsert(data, function(error, response) {
-          if (error) {
-            res.status(500).send(error);
-          } else {
-            res.send(response);
-          }
-        });
-      }
-
-      if (config.public.isUnsafeUpsert) {
-        upsertData();
-      } else {
-        ACL.checkAccessForContext(context, function(err, acl) {
-          if (err) { return res.status(500).send(err); }
-          if (acl.permission === 'DENY') { return res.status(403).send('Forbidden'); }
-          upsertData();
-        });
-      }
+      relationalUpsert.upsert(data, function(error, response) {
+        if (error) {
+          res.status(500).send(error);
+        } else {
+          res.send(response);
+        }
+      });
     });
   });
 
