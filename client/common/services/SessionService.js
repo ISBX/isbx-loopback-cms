57a05ece83dcc401f0a92c58e49e7b0947e58ad1
Kelly Chu <kchu@isbx.com>
2017-04-25 09:40:05 -0700

    Use Roles relationship when getting roles rather than accessing RoleMapping directly.

--- client/common/services/SessionService.js
+++ client/common/services/SessionService.js
@@ -41,32 +41,17 @@ angular.module('dashboard.services.Session', [
   };
 
   this.setSession = function(userInfo) {
-    return Utils.apiHelper('GET', 'Roles?access_token=' + userInfo.id)
+    var authModel = "Users";
+    if (config.authModel) authModel = config.authModel;
+    return Utils.apiHelper('GET', authModel + '/' + userInfo.userId + '/Roles?access_token=' + userInfo.id)
       .then(function(roles) {
-        return Utils.apiHelper('GET', 'RoleMappings?filter[where][principalId]='+userInfo.userId+'&access_token=' + userInfo.id)
-          .then(function(roleMappings) {
-            session = userInfo;
-            $cookies.put('username', userInfo.user.username);
-            $cookies.put('userId', userInfo.userId);
-            $cookies.put('accessToken', userInfo.id);
-            $cookies.put('session', JSON.stringify(session));
-            //get role name and description
-            for (var i in roleMappings) {
-              var roleMap = roleMappings[i];
-              var role = _.find(roles, {id: roleMap.roleId});
-              if (role) {
-                roleMap.name = role.name;
-                roleMap.description = role.description;
-              }
-            }
-            $cookies.put('roles', JSON.stringify(roleMappings));
-            return userInfo;
-
-          })["catch"](function() {
-          $cookies.put('session', null);
-          return $q.reject(arguments);
-        });
-
+        session = userInfo;
+        $cookies.put('username', userInfo.user.username);
+        $cookies.put('userId', userInfo.userId);
+        $cookies.put('accessToken', userInfo.id);
+        $cookies.put('session', JSON.stringify(session));
+        $cookies.put('roles', JSON.stringify(roles));
+        return userInfo;
       })["catch"](function() {
       $cookies.put('session', null);
       return $q.reject(arguments);
