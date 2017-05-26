015f4831cbc59a061206beecbdbb37f8528a9cc1
Charton Sapinoso <csapinoso@isbx.com>
2017-05-26 15:22:18 -0700

    fix issue with top-level routes with no real pages showing up blank page instead of checking role permission

--- client/common/services/SessionService.js
+++ client/common/services/SessionService.js
@@ -88,11 +88,21 @@ angular.module('dashboard.services.Session', [
     var state = toState.name;
     //dashboard.model.action.route
     var path = toParams.model; // model = config.nav[].path
+    var label = toParams.action;// action = config.nav[].label
     var roles = angular.fromJson($cookies.get('roles'));
 
-    var found = !_.isEmpty(path) ? _.find(nav, { path: path }) : _.find(nav, { state: state });
-    if(found) {
-      return DashboardService.hasAccess(roles, found);
+    if(!_.isEmpty(path)) { //check subnavs
+      var found = _.find(nav, { path: path });
+      if(found) {
+        if(!DashboardService.hasAccess(roles, found)) return false;
+        if(_.isArray(found.subnav) && !_.isEmpty(label)) {
+          var subnav = _.find(found.subnav, { label: label });
+          if(subnav) return DashboardService.hasAccess(roles, subnav);
+        }
+      }
+    } else { // check top nav using state
+      var found = _.find(nav, { state: state });
+      if(found) return DashboardService.hasAccess(roles, found);
     }
 
     var ctrlRoles = toState.data['roles'];
