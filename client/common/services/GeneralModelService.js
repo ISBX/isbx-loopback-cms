bc5a570e290d0da17e31ea24e25dba0e0c6e652d
Charton Sapinoso <charton.sapinoso@gmail.com>
2017-09-07 11:44:05 -0700

    refactor code

--- client/common/services/GeneralModelService.js
+++ client/common/services/GeneralModelService.js
@@ -285,10 +285,9 @@ angular.module('dashboard.services.GeneralModel', [
 
   this.validateRequiredFields = function(model, data) {
     var invalids = model.display.filter(function(item) {
-      var property;
       if (typeof item === 'string') {
-        property = model.properties[item];
-        return (property.required && !data[item]);
+        var property = model.properties[item];
+        return (property && property.required && !data[item]);
       } else if(typeof item === 'object') {
         return (item.required && item.options && item.options.relationship && _.isEmpty(data[item.options.relationship]));
       }
